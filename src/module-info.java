3c0642d792258bfd31a2843ff074a20bce2db162
la-pochoclera <beronalvaro@gmail.com>
2025-10-11 23:03:46 -0300

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module spaceInvaders {
-	requires java.desktop;
-}
--- src/principal/Main.java
+++
@@ -1,6 +0,0 @@
-package principal;
-
-public class Main {
-	public static void main(String[] args) {}
-	
-}
