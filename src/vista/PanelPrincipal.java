901aa37d0a315d819066c01660da2b3a993e2a06
la-pochoclera <beronalvaro@gmail.com>
2025-10-17 00:37:58 -0300

    background agregado

--- src/vista/PanelPrincipal.java
+++ src/vista/PanelPrincipal.java
@@ -1,6 +1,8 @@
 package vista;
 
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -8,6 +10,7 @@ import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import controlador.JuegoControlador;
@@ -17,6 +20,7 @@ public class PanelPrincipal extends JPanel{
 	private int ancho;
 	private int alto;
 	private SpriteNave spriteNave;
+	private Image fondo;
 	
 	public PanelPrincipal() {
 		this.ancho = 800;
@@ -31,6 +35,8 @@ public class PanelPrincipal extends JPanel{
 		juegoControlador = new JuegoControlador(ancho, alto, posicionNaveJugadorX, posicionNaveJugadorY, spriteNave);
 		interceptarTecla();
 		simularMovimiento();
+		fondo = new ImageIcon("sprites/background.png").getImage();
+		setLayout(null);
 	}
 	
 	private void interceptarTecla() {
@@ -70,4 +76,10 @@ public class PanelPrincipal extends JPanel{
 		});
 		gameLoop.start();
 	}
+
+	@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
+    }
 }
