5a6f3891b8a17168cdc9219ab6731d41f5da1964
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-10-25 21:46:35 -0300

    [FIX] dificultad velocidad

--- src/vista/VistaJuegoTerminado.java
+++ src/vista/VistaJuegoTerminado.java
@@ -1,10 +1,17 @@
 package vista;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class VistaJuegoTerminado extends JPanel {
     private Image fondo;
